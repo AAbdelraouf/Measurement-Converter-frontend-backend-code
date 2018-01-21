0af8636743e6f4bc6361be9bc727a498a2631b2c
Ahmed Abdelraouf <edbraouf@gmail.com>
2018-01-21 15:23:25 -0800

    files uploaded

--- mainPage/javascrptfile.js
+++ mainPage/javascrptfile.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){    
 console.log('welcome!') 
-
 $('#theAboutButton').on('click', function(){
     console.log('Welcome!')
     location.href = "https://msconversion.herokuapp.com/about";
