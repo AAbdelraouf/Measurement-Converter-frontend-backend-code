cecc4f5df4aee447487038a9c0ce80a707857e75
Ahmed Abdelraouf <edbraouf@gmail.com>
2018-01-21 15:30:48 -0800

    files uploaded

--- mainPage/javascrptfile.js
+++ mainPage/javascrptfile.js
@@ -1,32 +1,9 @@
 $(document).ready(function(){    
 console.log('welcome!') 
-
-// hidding the about paragraph page //
-$('#theAboutParagraph').hide()
     
-// The about page function //
-function theAboutbuttonAnimation(){
-    var switchTurns = true;    
-    $('#theAboutButton').on('click', function(){
-    if (switchTurns == true){
-    $("#wholeSection").fadeOut();
-    $('#theAboutParagraph').fadeIn();
-    $("#h1Message").html('About the Measurement conversion');
-    $('#theAboutButton').html('< Go Back');
-    $('#theAboutButton').css({backgroundColor: 'wheat'});
-    $('#h1Message').css({color: 'wheat'});
-    switchTurns = false;}
-    
-else if (switchTurns == false){
-    $("#wholeSection").fadeIn();
-    $('#theAboutParagraph').fadeOut();
-    $("#h1Message").html('Welcome to WLTS Measurment converter site');
-    $('#theAboutButton').html('About >');
-    $('#theAboutButton').css({backgroundColor: '#A8F72B'});
-    $('#h1Message').css({color: '#46DF46'});
-    switchTurns = true;}
-    })};
-    theAboutbuttonAnimation();
+$('#theAboutButton').on('click', function(){
+location.href = "https://msconversion.herokuapp.com/about";
+});
 
 // switching views function //
 function switchViewColors(buttonId, backgroundColor, colorCode){
